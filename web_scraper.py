8149bf8fdad2649782ca9748461595078c313d66
rener <reneripp@mial.uni-paderborn.de>
2022-06-19 10:59:06 +0200

    overworked web_scraper

--- web_scraper.py
+++ web_scraper.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import os
 
+options = Options()
+options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+options.add_argument("--headless")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
 service = Service(os.environ.get("CHROMEDRIVER_PATH"))
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(service=service, options=chrome_options)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 driver.get("https://www.javlibrary.com/en/")
 print(driver.page_source)
